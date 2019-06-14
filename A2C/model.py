35338a9e04a3ad564d61a71519c45efaddddf061
Patrick Riley <rileyp@bishopireton.org>
2019-06-14 16:03:53 -0400

    Redesigned A2C to use 2 networks, finished algorithm

--- A2C/model.py
+++ A2C/model.py
@@ -2,7 +2,66 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
 import copy
+
+class Actor(nn.Module):
+    def __init__(self,env):
+        super(Actor, self).__init__()
+
+        self.main = nn.Sequential(
+            nn.Linear(env.observation_space.shape[0],64),
+            nn.ELU(),
+            nn.Linear(64,128),
+            nn.ELU(),
+            nn.Linear(128,64),
+            nn.ELU(),
+            nn.Linear(64, env.action_space.n)
+        )
+
+    def forward(self, s):
+        logits = self.main(torch.FloatTensor(s))
+        dist = Categorical(logits=logits)
+        a = dist.sample().numpy()
+        return a
+
+    def get_action_probs(self, s):
+        s = self(s)
+        return F.softmax(self.main(s))
+
+    def get_log_p(self, s, a):
+        logits = self.main(torch.FloatTensor(s))
+        dist = Categorical(logits=logits)
+
+        log_p = dist.log_prob(torch.FloatTensor(np.array(a)))
+
+        return log_p
+
+
+class Critic(nn.Module):
+    def __init__(self,env):
+        super(Critic, self).__init__()
+
+        self.main = nn.Sequential(
+            nn.Linear(env.observation_space.shape[0],64),
+            nn.ELU(),
+            nn.Linear(64,128),
+            nn.ELU(),
+            nn.Linear(128,64),
+            nn.ELU(),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, s):
+        return self.main(torch.FloatTensor(s))
+
+        def get_state_value(self, s):
+            s = self(s)
+            return self.critic(s)
+
+
+
+
 
 
 
@@ -39,8 +98,6 @@ class A2C(nn.Module):
         return self.critic(s)
 
     def evaluate_actions(self, s):
-        s2 = s.deepcopy()
-        s3 = s.deepcopy()
-        action_probs = self.get_action_probs(s2)
-        state_value = self.get_state_value(s3)
+        action_probs = self.get_action_probs(s)
+        state_value = self.get_state_value(s)
         return action_probs, state_value
--- A2C/train.py
+++ A2C/train.py
@@ -3,38 +3,78 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model import A2C
+from model import *
 from history import History
 
-num_episodes = 1000
+max_steps = 10000
+num_steps = 128
+steps = 0
 
 gamma = .99
+epsilon = .1
 learn_rate = 2e-3
 
 env = gym.make('CartPole-v1')
 history = History()
 
-a2c = A2C(env)
-optimizer = torch.optim.Adam(a2c.parameters(), lr=learn_rate)
+actor = Actor(env)
+opt_A = torch.optim.Adam(actor.parameters(), lr=learn_rate)
+
+critic = Critic(env)
+opt_C = torch.optim.Adam(critic.parameters(), lr=learn_rate)
+
 
 def train():
-    for ep in range(num_episodes):
-        s = env.reset()
+    global steps
+    s = env.reset()
+    while steps < max_steps:
+        n_s = 0
+        #Collecting trajectories
+        while n_s < num_steps:
+            a = actor(s)
+            s2, r, done, _ = env.step(a)
+            history.store(s, a, r, done)
+            if done:
+                s = env.reset()
+            else:
+                s = s2
+            n_s += 1
+            steps += 1
+        n_s = 0
+        #----------#
+        #  Update  #
+        #----------#
+        states, actions, rewards, dones = history.get_history()
 
-        while True:
-            s = torch.FloatTensor(s).unsqueeze(0)
+        #Calculate the returns and normalize them
+        discount = 0
+        returns = [0] * len(rewards)
+        for i in reversed(range(len(rewards))):
+            if dones[i]:
+                returns[i] = rewards[i]
+            else:
+                returns[i] = rewards[i] + discount
+            discount = returns[i]*gamma
+        returns = torch.FloatTensor(returns)
 
-            action_probs = a2c.get_action_probs(s)
-            quit(action_probs)
-            a = action_probs.multinomial().data[0][0]
+        #------Policy Loss and Update------#
+        #Get advantage
+        advantage = returns - critic(s)
 
-            #quit(s.dtype)
+        logp = actor.get_log_p(states, actions)
+        policy_loss = -(logp * advantage).mean()
 
+        opt_A.zero_grad()
+        policy_loss.backward()
+        opt_A.step()
 
+        #------Value Function loss and update------#
+        v = F.mse_loss(returns, critic(states).squeeze(1))
 
+        opt_C.zero_grad()
+        v.backward()
+        opt_C.step()
 
-
-
-
+        history.clear()
 
 train()
